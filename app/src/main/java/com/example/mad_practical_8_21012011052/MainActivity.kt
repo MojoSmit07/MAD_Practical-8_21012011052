bdabab72fb03d51245623b960f1cb432836605b0
student <student@gnu.ac.in>
2023-09-21 08:53:28 +0530

    Practical 8

--- app/src/main/java/com/example/mad_practical_8_21012011052/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_8_21012011052/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.mad_practical_8_21012011052
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TimePicker
 import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
@@ -18,19 +20,28 @@ class MainActivity : AppCompatActivity() {
         val btn2 = findViewById<Button>(R.id.CancelBtn)
 
 
-        card2.visibility = View.GONE
+
+
 
         card1.setOnClickListener(){
 
         }
 
         btn1.setOnClickListener(){
-            card2.visibility = View.VISIBLE
+            TimePickerDialog(this, {tp,hour,minute -> setAlarmTime(hour,minute)}, 11,0,false).show()
+            //card2.visibility = View.VISIBLE
         }
 
         btn2.setOnClickListener(){
             card2.visibility = View.GONE
         }
+
+
+    }
+
+    fun setAlarmTime(hour: Int, minute:Int){
+
+       // card2.visibility = View.GONE
 
 
     }
