87c6c8aee7d64925a148cbfc5e6efbbb23780af7
student <student@gnu.ac.in>
2023-09-21 08:34:58 +0530

    Practical 8

--- app/src/main/java/com/example/mad_practical_8_21012011052/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical_8_21012011052/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.mad_practical_8_21012011052
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.material.card.MaterialCardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val card1 = findViewById<MaterialCardView>(R.id.card1)
+        val btn1 = findViewById<Button>(R.id.CreateBtn)
+        val card2 = findViewById<MaterialCardView>(R.id.card2)
+        val btn2 = findViewById<Button>(R.id.CancelBtn)
+
+
+        card2.visibility = View.GONE
+
+        card1.setOnClickListener(){
+
+        }
+
+        btn1.setOnClickListener(){
+            card2.visibility = View.VISIBLE
+        }
+
+        btn2.setOnClickListener(){
+            card2.visibility = View.GONE
+        }
+
+
     }
+
 }
